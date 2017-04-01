ff25e3f7555725d2b18a4d55c9e20d80b266fbc7
Donald Stufft <donald@stufft.io>
2017-04-01 13:32:14 -0400

    Add a hook for immediate feedback

--- browntruck/rebase.py
+++ browntruck/rebase.py
@@ -12,11 +12,76 @@
 
 import asyncio
 import http
+import json
 
 import aiohttp
-
 import gidgethub
 import gidgethub.aiohttp
+
+from aiohttp import web
+
+from browntruck.utils import verify_signature, InvalidSignature
+
+
+async def _check_pr(gh, pr_url):
+    # Determine if our PR is mergeable or not.
+    tries = 5
+    while True:
+        try:
+            pr_data = await gh.getitem(pr_url)
+        except gidgethub.BadRequest as exc:
+            if (isinstance(exc.status_code, http.HTTPStatus.NOT_FOUND)
+                    and tries > 0):
+                tries -= 1
+                await asyncio.sleep(1)
+            raise
+        else:
+            pr = pr_data
+            mergeable = pr["mergeable"]
+            if mergeable is not None:
+                break
+            else:
+                tries -= 1
+                await asyncio.sleep(1)
+
+    # Grab our labels out of GitHub's API
+    tries = 5
+    while True:
+        try:
+            issue_data = await gh.getitem(pr["issue_url"])
+            label_data = await gh.getitem(issue_data["labels_url"])
+        except gidgethub.BadRequest as exc:
+            if (isinstance(exc.status_code, http.HTTPStatus.NOT_FOUND)
+                    and tries > 0):
+                tries -= 1
+                await asyncio.sleep(1)
+            raise
+        else:
+            break
+    labels = {l["name"] for l in label_data}
+
+    if mergeable and "needs merged or rebased" in labels:
+        # The PR is now mergeable and no longer requires a merge or a
+        # rebase, so we'll go ahead and remove the label.
+        await gh.delete(issue_data["labels_url"],
+                        {"name": "needs merged or rebased"})
+    elif not mergeable and "needs merged or rebased" not in labels:
+        # The PR is not mergeable, so we'll mark it and add our comment
+        # to it explaining what needs to be done.
+        comment = (
+            "Hello!\n\n"
+            "I am an automated bot and I have noticed that this pull "
+            "request is not currently able to be merged. If you are "
+            "able to either merge the ``master`` branch into this "
+            "pull request or rebase this pull request against "
+            "``master`` then it will eligible for code review and "
+            "hopefully merging!"
+        )
+        await gh.post(issue_data["comments_url"], data={
+            "body": comment,
+        })
+        await gh.post(issue_data["labels_url"],
+                      data=["needs merged or rebased"])
 
 
 async def check_prs(app):
@@ -28,63 +93,35 @@ async def check_prs(app):
         )
 
         async for pr in gh.getiter(f"/repos/{app['repo']}/pulls?sort=updated"):
-            # Determine if our PR is mergeable or not.
-            tries = 5
-            while True:
-                try:
-                    pr_data = await gh.getitem(pr["url"])
-                except gidgethub.BadRequest as exc:
-                    if (isinstance(exc.status_code, http.HTTPStatus.NOT_FOUND)
-                            and tries > 0):
-                        tries -= 1
-                        await asyncio.sleep(1)
-                    raise
-                else:
-                    pr = pr_data
-                    break
-            mergeable = pr["mergeable"]
+            await _check_pr(pr["url"])
 
-            # If there isn't a mergeable state for this PR at all, then we will
-            # just skip processing it. Hopefully on the next pass it will have
-            # one and we can process it then.
-            if mergeable is None:
-                continue
 
-            # Grab our labels out of GitHub's API
-            tries = 5
-            while True:
-                try:
-                    issue_data = await gh.getitem(pr["issue_url"])
-                    label_data = await gh.getitem(issue_data["labels_url"])
-                except gidgethub.BadRequest as exc:
-                    if (isinstance(exc.status_code, http.HTTPStatus.NOT_FOUND)
-                            and tries > 0):
-                        tries -= 1
-                        await asyncio.sleep(1)
-                    raise
-                else:
-                    break
-            labels = {l["name"] for l in label_data}
+async def needs_rebase_hook(request):
+    payload = await request.read()
 
-            if mergeable and "needs merged or rebased" in labels:
-                # The PR is now mergeable and no longer requires a merge or a
-                # rebase, so we'll go ahead and remove the label.
-                await gh.delete(issue_data["labels_url"],
-                                {"name": "needs merged or rebased"})
-            elif not mergeable and "needs merged or rebased" not in labels:
-                # The PR is not mergeable, so we'll mark it and add our comment
-                # to it explaining what needs to be done.
-                comment = (
-                    "Hello!\n\n"
-                    "I am an automated bot and I have noticed that this pull "
-                    "request is not currently able to be merged. If you are "
-                    "able to either merge the ``master`` branch into this "
-                    "pull request or rebase this pull request against "
-                    "``master`` then it will eligible for code review and "
-                    "hopefully merging!"
-                )
-                await gh.post(issue_data["comments_url"], data={
-                    "body": comment,
-                })
-                await gh.post(issue_data["labels_url"],
-                              data=["needs merged or rebased"])
+    # Verify the payload against the signature
+    if (request.headers.get("X-Hub-Signature")
+            and request.app.get("github_payload_key")):
+        try:
+            verify_signature(
+                request.app["github_payload_key"],
+                request.headers["X-Hub-Signature"],
+                payload,
+            )
+        except InvalidSignature:
+            return web.json_response(
+                {"message": "Invalid signature"},
+                status=400,
+            )
+
+    data = json.loads(payload.decode(request.charset or "utf8"))
+
+    # We only care about a few different kinds of actions, the rest of them
+    # are not useful to us, so we'll no-op out quickly if it is one of them.
+    if (data.get("action") not in {"opened", "reopened", "synchronize"}):
+        return web.json_response({"message": "Skipped due to action"})
+
+    # Check our PR
+    await _check_pr(data["pull_request"]["url"])
+
+    return web.Response(status=204)
