b0b936f0dd018c19a986b73b75ddedd94a0ae271
Donald Stufft <donald@stufft.io>
2017-04-02 10:32:49 -0400

    Finish moving the news file status to Twisted

--- browntruck/news.py
+++ browntruck/news.py
@@ -10,15 +10,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import io
+import re
+
+import treq
+import unidiff
+
 from txghbot import IWebhook
 from twisted.internet import defer
+from twisted.logger import Logger
 from twisted.plugin import IPlugin
 from zope.interface import implementer
 
-from .utils import getGitHubAPI
+from .utils import getGitHubAPI, Retry
 
+
+NEWS_FILE_CONTEXT = "news-file/pr"
+
+HELP_URL = "https://pip.pypa.io/en/latest/development/#adding-a-news-entry"
 
 ACTIONS = {"labeled", "unlabeled", "opened", "reopened", "synchronize"}
+
+
+log = Logger()
+
+
+_news_fragment_re = re.compile(
+    r"news/[^\./]+\.(removal|feature|bugfix|doc|vendor|trivial)$"
+)
 
 
 @implementer(IPlugin, IWebhook)
@@ -32,4 +51,72 @@ class NewsFileWebhook:
         return defer.ensureDeferred(self.hook(eventName, eventData, requestID))
 
     async def hook(self, eventName, eventData, requestID):
+        log.info("Processing {eventData[number]}", eventData=eventData)
+
         gh = getGitHubAPI()
+
+        # First things first, we want to fetch the whole PR data from GitHub
+        # instead of relying on what was sent in the Hook. This will ensure
+        # that we have the latest data as well as be more secure.
+        for attempt in Retry():
+            with attempt:
+                prData = await gh.getitem(eventData["pull_request"]["url"])
+
+        # Next, we want to fetch the issue data, we need this seperately from
+        # the PR data because not everything is contained within the PR since
+        # GitHub models PRs as issues with attached code.
+        for attempt in Retry():
+            with attempt:
+                issueData = await gh.getitem(prData["issue_url"])
+
+        # We also need our label data since a PR can be marked as trivial which
+        # skips the requirement for a news file.
+        for attempt in Retry():
+            with attempt:
+                labelData = await gh.getitem(issueData["labels_url"])
+        labels = {l["name"] for l in labelData}
+
+        # Finally, we need to fetch the diff from GitHub so that we can see if
+        # the user has added any news fragments to our news directory.
+        diff = unidiff.PatchSet(io.StringIO(
+            await treq.text_content(await treq.get(prData["diff_url"]))))
+
+        # Determine if the status check for this PR is passing or not and
+        # update the status check to account for that.
+        if ("trivial" in labels
+                or any(f.is_added_file for f in diff
+                       if _news_fragment_re.search(f.path))):
+            await gh.post(
+                prData["statuses_url"],
+                data={
+                    "context": NEWS_FILE_CONTEXT,
+                    "target_url": HELP_URL,
+                    "state": "success",
+                    "description":
+                        "News files updated and/or change is trivial.",
+                },
+            )
+
+            log.info("{prData[number]}: {status}",
+                     prData=prData,
+                     status="news file updated and/or ignored")
+        else:
+            await gh.post(
+                prData["statuses_url"],
+                data={
+                    "context": NEWS_FILE_CONTEXT,
+                    "target_url": HELP_URL,
+                    "state": "failure",
+                    "description":
+                        "Missing either a news entry or a trivial file/label.",
+                },
+            )
+
+            log.info("{prData[number]}: {status}",
+                     prData=prData,
+                     status="news file was not updated",
+                     labels=labels,
+                     files=[
+                        {"path": f.path, "is_added_file": f.is_added_file}
+                        for f in diff
+                     ])
--- browntruck/utils.py
+++ browntruck/utils.py
@@ -10,8 +10,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import attr
+
 from gidgethub.treq import GitHubAPI
 
 
 def getGitHubAPI():
     return GitHubAPI("BrownTruck")  # , oauth_token=MY_TOKEN)
+
+
+@attr.s
+class Attempt:
+
+    number = attr.ib()
+    retried = attr.ib()
+    catch = attr.ib()
+    successful = attr.ib(default=None, init=False)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Our call was successful, so we can mark ourselves as successful.
+        if exc_type is None:
+            self.successful = True
+        # If our current attempt is less than our total number of retries, then
+        # we want to suppress the exception, our outer loop will ensure that
+        # we get called again.
+        elif issubclass(exc_type, self.catch) and self.number < self.retries:
+            return True
+
+
+@attr.s
+class Retry:
+
+    catch = attr.ib(default=(Exception,))
+    retries = attr.ib(default=5)
+
+    def __iter__(self):
+        # We want to effectively run forever until our latest attempt tells us
+        # to stop. When we reach our maximum number of retries our Attempt
+        # class will let the exception propagate instead of silencing it, so
+        # that will break us out of the loop.
+        number, attempt = 0, None
+        while attempt is None or not attempt.successful:
+            number += 1
+            attempt = Attempt(number=number, retries=self.retries,
+                              catch=tuple(self.catch))
+            yield attempt
