5eb02a7d41a48cd7b1f38df3ea97e5b913f26d23
Pradyun S. Gedam <pradyunsg@users.noreply.github.com>
2017-08-06 21:53:18 +0530

    Fix the check for updating news files

--- browntruck/hooks/news.py
+++ browntruck/hooks/news.py
@@ -78,7 +78,7 @@ class NewsFileWebhook:
         # Determine if the status check for this PR is passing or not and
         # update the status check to account for that.
         if ("trivial" in labels
-                or any(f.is_added_file for f in diff
+                or any(not f.is_removed_file for f in diff
                        if _news_fragment_re.search(f.path))):
             await gh.post(
                 prData["statuses_url"],
